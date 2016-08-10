c1eb95a9ce61ef35df010de41a0ccf438e7d7f26
Erik Martins <erik.kire@outlook.com>
2016-08-10 00:37:16 -0300

    Initial Commit

---
+++ Houd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Houd
+//
+//  Created by Erik Martins on 10/08/16.
+//  Copyright © 2016 Erik Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
